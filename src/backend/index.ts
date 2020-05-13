89dc74405de9abf5dfc04be7183d0f4d0132e1cf
Sarah Ohlin <sarahohlin1@gmail.com>
2020-05-12 22:39:27 -0400

    feat: add express-session

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,5 +1,6 @@
 require('dotenv').config();
 import express from 'express';
+import session from 'express-session';
 import appRoot from 'app-root-path';
 
 import './db/init';
@@ -7,6 +8,10 @@ import './db/init';
 import api from './api';
 
 const app = express();
+app.use(session({
+	secret: process.env.SECRET,
+}));
+
 app.use('/assets', express.static(appRoot.resolve('assets')));
 app.use('/js', express.static(appRoot.resolve('build/frontend/js')));
 app.use('/css', express.static(appRoot.resolve('build/frontend/css')));
