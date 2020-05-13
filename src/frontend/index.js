25176ba5ff9638b96030348b9b6dd6fe3654013b
Sarah Ohlin <sarahohlin1@gmail.com>
2020-05-13 00:52:49 -0400

    chore: add bootstrap

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -3,7 +3,11 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import LoadScript from 'vue-plugin-load-script';
 import Snotify, { SnotifyPosition } from 'vue-snotify';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
 import 'vue-snotify/styles/simple.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import store from './store';
 import router from './router';
@@ -12,6 +16,8 @@ Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 Vue.use(LoadScript);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 Vue.use(Snotify, {
 	toast: {
 		position: SnotifyPosition.rightBottom,
