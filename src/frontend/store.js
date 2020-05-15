668ea025fabb5ea57cc45c9e3407f1cdb3611d45
Sarah Ohlin <sarahohlin1@gmail.com>
2020-05-15 19:01:00 -0400

    feat: implement patAndUpdate action

--- src/frontend/store.js
+++ src/frontend/store.js
@@ -12,23 +12,43 @@ const store = new Vuex.Store({
 	mutations: {
 		setPatUsers(state, users) {
 			state.patUsers = users;
-		}
+		},
+		updatePatUser(state, name, newpats) {
+			let i = state.patUsers.findIndex(e => e.name === name);
+			if (i != -1) {
+				state.patUsers[i] = { name, pats: newpats };
+			}
+		},
 	},
 	actions: {
 		updatePatUsers({ commit }) {
 			fetch('/api/pat')
 			.catch((err) => {
-				Vue.$snotify.error(err, 'couldn\'t get the fwiggin,, pats');
+				Vue.$snotify.error(err, 'error gettin pats');
 			})
 			.then(res => res.json())
 			.then(json => {
-				let users = []
+				let users = [];
 				for (let user of json) {
 					users.push({ name: user.name, pats: user.pats });
 				}
 				commit('setPatUsers', users);
 			});
-		}
+		},
+		patAndUpdate({ commit }, { name }) {
+			fetch('/api/pat', {
+				method: 'post',
+				body: JSON.stringify({ name }),
+				headers: { 'Content-Type': 'application/json' }
+			})
+			.catch(err => {
+				Vue.$snotify.error(err, 'cat petting error');
+			})
+			.then(res => res.json())
+			.then(json => {
+				commit('updatePatUser', json.name, json.pats);
+			});
+		},
 	},
 	getters: {
 		pats(state) {
