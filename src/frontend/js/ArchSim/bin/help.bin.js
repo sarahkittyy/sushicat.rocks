0549a52a8038bf813c5b5632e3a75da8191bd41c
Sarah Ohlin <sarahohlin1@gmail.com>
2020-06-04 01:08:42 -0400

    chore: add place to report bugs in help command

--- src/frontend/js/ArchSim/bin/help.bin.js
+++ src/frontend/js/ArchSim/bin/help.bin.js
@@ -2,5 +2,8 @@ module.exports = function help(sim, args) {
 	return [
 		`-- commands --`,
 		...Object.keys(sim.bin),
+		`-- ........ --`,
+		`report bugs @ github.com/sarahkittyy/sushicat.rocks`,
+		``
 	].join('\n');
 }
