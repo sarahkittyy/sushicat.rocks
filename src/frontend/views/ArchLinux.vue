ccafcd7a312131b640e3c4d60d74331c553d05b2
Sarah Ohlin <sarahohlin1@gmail.com>
2020-06-03 22:29:07 -0400

    feat: base arch linux terminal settings

--- src/frontend/views/ArchLinux.vue
+++ src/frontend/views/ArchLinux.vue
@@ -1,6 +1,10 @@
 <template>
-<div>
-	
+<div class="arch-container font-cp437">
+	<div id="terminal">
+		<span v-for="n in 100">
+			hi <br />
+		</span>
+	</div>
 </div> 
 </template>
 
@@ -11,5 +15,45 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+@use '~@/common';
+
+.arch-container {
+	display: flex;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	
+	font-size: 22px;
+	
+	justify-content: center;
+	
+	background-color: black;
+	
+	#terminal {
+		color: #ccc;
+		overflow-y: scroll;
+		width: 90%;
+		margin-top: 5%;
+		margin-bottom: 5%;
+		
+		scrollbar-width: thin;
+		scrollbar-color: common.$dark-grey common.$grey;
+		
+		&::-webkit-scrollbar {
+			width: 8px;
+		}
+		&::-webkit-scrollbar-track {
+			background: black;
+		}
+		&::-webkit-scrollbar-thumb {
+			background-color: common.$dark-grey;
+			border-radius: 10px;
+			border: 1px solid common.$grey;
+		}
+	}
+}
 
 </style>
