69eb63a9a1e0455de56da3186aa90f2cb3c4cddc
Sarah Ohlin <sarahohlin1@gmail.com>
2020-06-03 20:47:45 -0400

    fix: clean up CornerNav animation

--- src/frontend/components/CornerNav.vue
+++ src/frontend/components/CornerNav.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="corner-toast font-comic-sans" @mouseover="hovering = true" @mouseleave="hovering = false">
+<div class="corner-toast font-comic-sans" @mouseenter="mouseenter" @mouseleave="mouseleave">
 	<slot name="small" v-if="!hovering" />
 	<slot name="large" v-else />
 </div>
@@ -11,7 +11,20 @@ export default {
 	data() {
 		return {
 			hovering: false,	
+			timeout: null
 		};
+	},
+	methods: {
+		mouseenter() {
+			console.log('over');
+			clearTimeout(this.timeout);
+			this.timeout = setTimeout(() => {
+				this.hovering = true;
+			}, 200);
+		},
+		mouseleave() {
+			this.hovering = false;
+		}
 	}
 };
 </script>
@@ -25,9 +38,9 @@ export default {
 	right: 0;
 	bottom: 0;
 	
-	min-width: 20px;
-	min-height: 20px;
-
+	width: 50px;
+	height: 30px;
+	
 	border-top: 2px solid black;
 	border-left: 2px solid black;
 	background-color: common.$grey;
@@ -36,7 +49,7 @@ export default {
 	padding: 10px;
 	
 	transform-origin: 100% 100%;
-	transition: all 500ms;
+	transition: all 200ms ease-out;
 	
 	&:hover {
 		width: 15%;
