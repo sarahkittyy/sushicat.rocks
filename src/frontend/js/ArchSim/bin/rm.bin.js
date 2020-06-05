50e1572c5b63f4e4f4899d8a3dd78940b1550cc2
Sarah Ohlin <sarahohlin1@gmail.com>
2020-06-04 20:39:07 -0400

    feat: rm command

---
+++ src/frontend/js/ArchSim/bin/rm.bin.js
@@ -0,0 +1,13 @@
+module.exports = function rm(sim, args) {
+	let ret = [];
+	for(let file of args.split(' ')) {
+		let res = sim.fs.rm(file);
+		if (res.code === 404) {
+			ret.push(`rm: cannot remove '${file}': No such file or directory`);
+		} else if (res.code === 400) {
+			ret.push(`rm: cannot remove '${file}': Is a directory`);
+		}
+	}
+	
+	return ret.join('\n');
+}
