e8ae2bd7f843b5dba2a26c5d1cef869a41c7a629
Sarah Ohlin <sarahohlin1@gmail.com>
2022-06-01 19:14:33 -0400

    feat: /discord endpoint

--- src/frontend/router.js
+++ src/frontend/router.js
@@ -12,6 +12,9 @@ import NyoomCars from './views/NyoomCars';
 
 const routes = [
 	{ path: '/', redirect: '/home' },
+	{ path: '/discord', beforeEnter(to, from, next) {
+		window.location.replace("https://discord.gg/ewWPufHxWK");
+	}},
 	{ path: '/home', component: Home },
 	{ path: '/arf', component: Arf },
 	{ path: '/arch', component: ArchLinux },
